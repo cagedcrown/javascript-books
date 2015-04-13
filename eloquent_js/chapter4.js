99577532c5e33b5be9d0aad83f9f6bc5c54d464c
Maury Lindo <maury@cagedcrown.com>
2015-04-13 17:24:42 -0400

    added answer to question 1

--- eloquent_js/chapter4.js
+++ eloquent_js/chapter4.js
@@ -1,12 +1,25 @@
 // Question 1: Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
-
-var range = function(start, end) {
-  // do something
-  // return
-}
-
 // Part 2: Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the previous program and see whether it does indeed return 55.
 
+function range(begin, end, step) {
+  collection = [];
+  if(!step)
+    step = 1;
+  if(begin < end)
+    for(var i = begin; i <= end; i = i + step)
+        collection.push(i);
+    else if(begin > end)
+        for(var i = begin; i >= end; i = i + step)
+            collection.push(i);
+  return collection;
+}
+
+function sum(collection) {
+  sum = 0;
+  for (var i = 0; i < collection.length; i++)
+    sum += collection[i];
+  return sum;
+}
 
 // Question 2: Reversing and Array
 
