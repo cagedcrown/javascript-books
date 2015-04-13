e2482a608a190d563f28816deefc31fe261cd172
Maury Lindo <maury@cagedcrown.com>
2015-04-12 23:08:35 -0400

    recent books added to readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# javascript-books
+# Javascript Books / Tutorials / Resources
+
+## Books
+Eloquent JavaScript
+JavaScript and jQuery by Jon Duckett
+You Don't Know JS
+Object Orientated JavaScript
+JavaScript the Good Parts
+JavaScript Ninja
+
+## Tutorials
+Angular
+
+## Resources
+The Odin Project
---
+++ eloquent_js/chapter4.js
@@ -0,0 +1,8 @@
+// Question 1: Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+
+var range = function(start, end) {
+  // do something
+  // return
+}
+
+// Part 2: Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the previous program and see whether it does indeed return 55.
