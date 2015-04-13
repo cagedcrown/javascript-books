2b0ff45fa90eeaa490e400307a312ab8cd6bf623
Maury Lindo <maury@cagedcrown.com>
2015-04-12 23:14:28 -0400

    question 2 added

--- eloquent_js/chapter4.js
+++ eloquent_js/chapter4.js
@@ -6,3 +6,23 @@ var range = function(start, end) {
 }
 
 // Part 2: Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the previous program and see whether it does indeed return 55.
+
+
+// Question 2: Reversing and Array
+
+function reverseArray(collection) {
+  reversed = [];
+  for(var i = collection.length - 1; i >= 0; i--) {
+    reversed.push(collection[i]);
+  }
+  return reversed;
+}
+
+function reverseArrayInPlace(collection) {
+  length = collection.length - 1;
+  for(var i = 0; i < length / 2); i++) {
+    temp = collection[i];
+    collection[i] = collection[length - i];
+    collection[length - i] = temp;
+  }
+}
