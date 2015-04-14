d4a7d62992a35728e79b75f061c17adf9602d492
Maury Lindo <maury@cagedcrown.com>
2015-04-13 21:58:18 -0400

    added solutions to question 3 and 4

--- eloquent_js/chapter4.js
+++ eloquent_js/chapter4.js
@@ -39,3 +39,63 @@ function reverseArrayInPlace(collection) {
     collection[length - i] = temp;
   }
 }
+
+// Question 3: Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as argument, and write a listToArray function that produces an array from a list.
+function arrayToList(array) {
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--)
+    list = {value: array[i], rest: list};
+  return list;
+}
+
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.rest)
+    array.push(node.value);
+  return array;
+}
+
+function prepend(value, list) {
+  return {value: value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
+
+console.log(arrayToList([10, 20])); // {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30]))); // [10, 20, 30]
+console.log(prepend(10, prepend(20, null))); // {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1)); // 20
+
+// Question 4: Deep Comparison
+function deepEqual(a, b) {
+  if (a === b) return true;
+
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object")
+    return false;
+
+  var propsInA = 0, propsInB = 0;
+
+  for (var prop in a)
+    propsInA += 1;
+
+  for (var prop in b) {
+    propsInB += 1;
+    if (!(prop in a) || !deepEqual(a[prop], b[prop]))
+      return false;
+  }
+  
+  return propsInA == propsInB;
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj)); // true
+console.log(deepEqual(obj, {here: 1, object: 2})); // false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2})); // true
